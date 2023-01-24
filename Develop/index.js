29036e3c12bed8d9d5323376c70cf386aee9bf37
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-01-23 18:25:09 -0800

    Added dry function get app intialize and Test questions to test user input

--- Develop/index.js
+++ Develop/index.js
@@ -3,13 +3,34 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Question 1",
+        message: "Question 1 Answer",
+    },
+    {
+        type: "input",
+        name: "Question 2",
+        message: "Question 2 Answer",
+    },
+    {
+        type: "input",
+        name: "Question 3",
+        message: "Question 3 Answer",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("App is running");
+    inquirer.prompt(questions).then(input => {
+        console.log(input);
+    });
+}
 
 // Function call to initialize app
 init();
